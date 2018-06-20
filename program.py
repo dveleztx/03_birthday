a8a875437748bbd78aac640a108dc130fcf86bd3
dveleztx <dveleztx@gmail.com>
2018-06-20 14:04:07 -0700

    defined a couple of functions, imported datetime, print header/results

--- program.py
+++ program.py
@@ -1,6 +1,38 @@
+import datetime
+
+
+def print_header():
+    print('-------------------------')
+    print('     BIRTHDAY APP')
+    print('-------------------------')
+    print()
+
 
 def get_birthday_from_user():
-    pass
+    print('When were you born?' )
+    year = int(input('Year [YYYY]: '))
+    month = int(input('Month [MM]: '))
+    day = int(input('Day [DD]: '))
+
+    birthday = datetime.date(year, month, day)
+    return birthday
+
 
 def compute_days_between_dates():
     pass
+
+
+def print_birthday_information():
+    pass
+
+
+def main():
+    print_header()
+    bday = get_birthday_from_user()
+    print(bday)
+    #now = None
+    #number_of_days = compute_days_between_dates(bday)
+    #print_birthday_information()
+
+
+main()
